27a6812cd108829b73336d9b67b32829a640a964
smorin4 <66053234+smorin4@users.noreply.github.com>
2020-12-07 21:27:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -83,45 +83,7 @@ void mergeSort(int arr[], int l, int r){
     }
 }
 
-void swap(int *a, int *b) {
-  int t = *a;
-  *a = *b;
-  *b = t;
-}
-
-int partition(int array[], int low, int high) {
-  // Select the pivot element
-  int pivot = array[high];
-  int i = (low - 1);
-
-  // Put the elements smaller than pivot on the left 
-  // and greater than pivot on the right of pivot
-  for (int j = low; j < high; j++) {
-    if (array[j] <= pivot) {
-      i++;
-      swap(&array[i], &array[j]);
-    }
-  }
-  swap(&array[i + 1], &array[high]);
-  return (i + 1);
-}
-
-void quickSort(int array[], int low, int high) {
-  if (low < high) {
-    // Select pivot position and put all the elements smaller 
-    // than pivot on left and greater than pivot on right
-    int pi = partition(array, low, high);
-
-    // Sort the elements on the left of pivot
-    quickSort(array, low, pi - 1);
-
-    // Sort the elements on the right of pivot
-    quickSort(array, pi + 1, high);
-  }
-}
-
-
-/*int partition(int array[], int low, int  high) {
+int partition(int array[], int low, int  high) {
     int pivot = array[low]; 
     int up = low; 
     int down = high;
@@ -145,15 +107,15 @@ void quickSort(int array[], int low, int high) {
     }
      swap(&array[low], &array[down]);
     return down;
-}*/
+}
 
-/*void quickSort(int arr[], int low, int high) {
+void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pivot = partition(arr, low, high);
         quickSort(arr, low, pivot - 1);
         quickSort(arr, pivot + 1, high);
     }
-}*/
+}
 
 int main()
 {
